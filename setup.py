2e4606179e21e79a24500ac7a94591242b76b1ff
Dustin Dorroh <dustindorroh@gmail.com>
2012-08-08 00:57:14 -0700

    Initial commit of the pyro source, config, and setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#  setup.py
+#  
+#  Dustin Dorroh <dustin.dorroh@decisionsciencescorp.com>
+#  
+
+from distutils.core import setup
+
+setup(name='pyro',
+	version='0.2',
+	description='Python file rotater',
+	author='Dustin Dorroh',
+	author_email='dustin.dorroh@decisionsciencescorp.com',
+	maintainer='Dustin Dorroh',
+	maintainer_email='dustin.dorroh@decisionsciencescorp.com',
+	scripts=['pyro'],
+	data_files=[('/etc',['pyrorc'])]
+	)
